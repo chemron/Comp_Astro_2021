03dd8eabc530ea9ba640be6374dff64a9d6df429
chemron <camerontasmith@gmail.com>
2021-02-24 11:52:33 +1100

    getting fortran to work

---
+++ Assingment_1/assignment_1.f90
@@ -0,0 +1,9 @@
+program assingment_1
+    implicit none
+    integer, parameter :: n_max = 100
+    real :: x(n_max), v(n_max), m(n_max), l(n_max), rho(n_max), u(n_max), P(n_max), c(n_max)
+    ! position (x), velocity (v), mass (m), smoothing length (l), density (rho), internal
+    ! energy (u), pressure (P), and sound speed (c)
+    print*, 'Hello World'
+
+end program assingment_1
---
+++ test.f90
@@ -0,0 +1,7 @@
+program myprog
+
+    x = -3.0
+    y = 2.0 * x + 3.0
+    print* , y
+
+end program
